de7530e798cdc73f9285951218c349dc2216930a
Melina <84160185+melisciar@users.noreply.github.com>
2022-10-08 20:20:58 -0300

    Ahora el jugador puede moverse hacia arriba y abajo. También está hecha la validación para que no pueda salirse de escena

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter
 
 class App:
+    __STEP = 1
     __POS_JUGADOR_X = 10
     __pos_jugador = 0
 
@@ -8,13 +9,14 @@ class App:
         self.root = tkinter.Tk()
         self.root.title("Ping Pong")
         self.root.geometry("750x500")
-        #self.root.update()
-        #self.__ANCHO_VENTANA = self.root.winfo_width()
-        #self.__ALTO_VENTANA = self.root.winfo_height()
+        self.root.update()
+        self.__ANCHO_VENTANA = self.root.winfo_width()
+        self.__ALTO_VENTANA = self.root.winfo_height()
         self.root.resizable(0, 0)
         self.configurar_escena()
         self.crear_jugador()
-        #self.root.bind("<KeyPress-Down>", self.mover_jugador)
+        self.root.bind("<KeyPress-Down>", self.mover_jugador)
+        self.root.bind("<KeyPress-Up>", self.mover_jugador)
         self.root.mainloop()
         
 
@@ -27,11 +29,12 @@ class App:
         print("HOLA")
         self.jugador = self.escena.create_rectangle(self.__POS_JUGADOR_X, 0, 40, 100, fill="#33FF00")
 
-    """def mover_jugador(self, event):
-        self.__pos_jugador += 1 if event.keysysm == "Down" else -1
-        self.__pos_jugador = max(0, min(self.__ALTO_VENTANA, self.__pos_jugador))
-        self.escena.move(self.jugador, 0, self.__pos_jugador)
-        self.escena.update()"""
+    def mover_jugador(self, event):
+
+        self.__pos_jugador += self.__STEP if event.keysym == "Down" else -self.__STEP
+        self.__pos_jugador = max(0, min(self.__ALTO_VENTANA - 100, self.__pos_jugador))
+        self.escena.coords(self.jugador, self.__POS_JUGADOR_X, self.__pos_jugador, 40, self.__pos_jugador + 100)
+        self.escena.update()
 
 if __name__ == "__main__":
     app = App()
