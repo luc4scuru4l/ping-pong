dcb1a1a0a662c8a540d1c6a7f6e37f7c45bddc8c
Melina <84160185+melisciar@users.noreply.github.com>
2022-10-08 20:44:25 -0300

    Cambié jugador por barra, creo que va a ser más claro

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import tkinter
 
 class App:
-    __STEP = 1
-    __POS_JUGADOR_X = 10
-    __pos_jugador = 0
+    #CONSTANTES ASOCIADAS A LA BARRA
+    __BARRA_VEL = 10
+    __POS_BARRA_X = 10
+
+
+    __pos_barra = 0
 
     def __init__(self):    
         self.root = tkinter.Tk()
@@ -14,9 +17,9 @@ class App:
         self.__ALTO_VENTANA = self.root.winfo_height()
         self.root.resizable(0, 0)
         self.configurar_escena()
-        self.crear_jugador()
-        self.root.bind("<KeyPress-Down>", self.mover_jugador)
-        self.root.bind("<KeyPress-Up>", self.mover_jugador)
+        self.crear_barra()
+        self.root.bind("<KeyPress-Down>", self.mover_barra)
+        self.root.bind("<KeyPress-Up>", self.mover_barra)
         self.root.mainloop()
         
 
@@ -25,15 +28,13 @@ class App:
         self.escena = tkinter.Canvas(self.root, width=750, height=500, bg="black")
         self.escena.pack()
     
-    def crear_jugador(self):
-        print("HOLA")
-        self.jugador = self.escena.create_rectangle(self.__POS_JUGADOR_X, 0, 40, 100, fill="#33FF00")
+    def crear_barra(self):
+        self.barra = self.escena.create_rectangle(self.__POS_BARRA_X, 0, 40, 100, fill="#33FF00")
 
-    def mover_jugador(self, event):
-
-        self.__pos_jugador += self.__STEP if event.keysym == "Down" else -self.__STEP
-        self.__pos_jugador = max(0, min(self.__ALTO_VENTANA - 100, self.__pos_jugador))
-        self.escena.coords(self.jugador, self.__POS_JUGADOR_X, self.__pos_jugador, 40, self.__pos_jugador + 100)
+    def mover_barra(self, event):
+        self.__pos_barra += self.__BARRA_VEL if event.keysym == "Down" else -self.__BARRA_VEL
+        self.__pos_barra = max(0, min(self.__ALTO_VENTANA - 100, self.__pos_barra))
+        self.escena.coords(self.barra, self.__POS_BARRA_X, self.__pos_barra, 40, self.__pos_barra + 100)
         self.escena.update()
 
 if __name__ == "__main__":
