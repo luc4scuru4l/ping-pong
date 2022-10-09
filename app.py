b33abd9d91b351531c298c967d542c0ca1de6686
Melina <84160185+melisciar@users.noreply.github.com>
2022-10-09 16:17:27 -0300

    Ahora choca con el piso, el techo y la pared. Vamos por la barra.

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ class Barra(Figura):
             *self.get_posicion(), 
             fill=color
             )
+        self.dibujar()
 
     def corregir_posicion(self, y):
         return max(0, min(self.__MAX_POS, y))
@@ -63,7 +64,7 @@ class Barra(Figura):
         )
         self.dibujar()
 
-class Pelota:
+class Pelota(Figura):
     __POS_X_INICIAL = 100
     __POS_Y_INICIAL = 100
     __RADIO = 25
@@ -76,28 +77,32 @@ class Pelota:
         self.__lado_izq_y = self.__POS_Y_INICIAL
         self.__lado_der_x = self.__lado_izq_x + self.__DIAMETRO
         self.__lado_der_y = self.__lado_izq_y + self.__DIAMETRO
-        self.canvas = canvas
-        #print(self.canvas.winfo_width())
+        Figura.__init__(
+            self,
+            canvas,
+            self.__lado_izq_x,
+            self.__lado_izq_y,
+            self.__lado_der_x,
+            self.__lado_der_y,)
         self.id = canvas.create_oval(
-            self.__lado_izq_x, 
-            self.__lado_izq_y, 
-            self.__lado_der_x, 
-            self.__lado_der_y, 
+            *self.get_posicion(), 
             fill=color
             )
+        self.dibujar()
     def mover(self):
-        self.__lado_izq_x += self.__velx
-        self.__lado_izq_y += self.__vely
-        self.__lado_der_x += self.__velx
-        self.__lado_der_y += self.__vely
-        self.rebote()
-        self.canvas.coords(
-            self.id, 
-            self.__lado_izq_x, 
-            self.__lado_izq_y, 
-            self.__lado_der_x, 
-            self.__lado_der_y
-            )
+        #print(self.get_posicion())
+        self.rebota_en_y()
+        self.rebota_en_x()
+        self.actual = self.get_posicion()
+        self.x = self.actual[0] + self.__velx
+        self.y = self.actual[1] + self.__vely
+        self.set_posicion(
+            self.x,
+            self.y,
+            self.x + self.__DIAMETRO,
+            self.y + self.__DIAMETRO
+        )
+        self.dibujar()
         self.canvas.after(10, self.mover)
     def reflejar_x(self, n: int):
         self.__velx *= -1
@@ -105,14 +110,32 @@ class Pelota:
         self.__lado_der_x += n
     def reflejar_y(self, n: int):
         self.__vely *= -1
-        self.__lado_izq_y += n
-        self.__lado_der_y += n
+        self.actual = self.get_posicion()
+        self.y = self.actual[1] + n
+        self.set_posicion(
+            self.actual[0],
+            self.actual[1] + n,
+            self.actual[2],
+            self.y + self.diametro()
+        )
     def radio(self):
         return self.__RADIO
     def choca_con_pared(self):
-        return self.__lado_der_x > self.canvas.winfo_width()
-    def rebote(self):
+        return self.get_posicion()[2] > self.canvas.winfo_width()
+    def choca_con_techo(self):
+        return self.get_posicion()[1] < 0
+    def choca_con_piso(self):
+        return self.get_posicion()[3] > self.canvas.winfo_height()
+    def rebota_en_y(self):
+        if self.choca_con_techo():
+            print("CHOCÓ CON TECHO")
+            self.reflejar_y(-self.get_posicion()[1])
+        if self.choca_con_piso():
+            print("CHOCÓ CON PISO")
+            self.reflejar_y(self.canvas.winfo_height() - self.get_posicion()[3])
+    def rebota_en_x(self):
         if self.choca_con_pared():
+            print("CHOCÓ CON PARED")
             self.reflejar_x(self.canvas.winfo_width() - self.__lado_der_x)
     def diametro(self):
         return self.__DIAMETRO
@@ -130,8 +153,7 @@ class App:
         self.configurar_escena()
         self.escena.update()
         self.barra = Barra(self.escena, "#33FF00")
-        self.barra.dibujar()
-        self.pelota = Pelota(self.escena, "white")
+        self.pelota = Pelota(self.escena, "#33FF00")
         self.pelota.mover()
         self.root.bind("<KeyPress-Down>", self.barra.mover)
         self.root.bind("<KeyPress-Up>", self.barra.mover)
